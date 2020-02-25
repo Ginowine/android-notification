7d5142a83ab4abfba48081efd153e029be3b4dc5
ginowine <emailosahon@yahoo.com>
2020-02-25 13:13:35 +0100

    pending Intent works fine

--- app/src/main/java/com/example/notifyme/MainActivity.java
+++ app/src/main/java/com/example/notifyme/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.notifyme;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -34,9 +36,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public NotificationCompat.Builder getNotificationBuilder(){
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
                 .setContentTitle("You've been notified!")
                 .setContentText("This is your notification text.")
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true)
                 .setSmallIcon(R.drawable.ic_android);
 
         return notifyBuilder;
